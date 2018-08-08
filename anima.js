fe7ecbe948132af43334f443eb44f698b555527a
vinkho.c@gmail.com <=mQiXZ#FAf*^!@RUZzW9TX}z>
2018-08-08 16:48:02 +0200

    getActivePlayerEnergyPool works

--- anima.js
+++ anima.js
@@ -11,9 +11,9 @@ function states()
       },
       
       200: {
-          description: _('${actplayer} must move a character'),
-          descriptionmyturn: _('${you} must move a character'),
-          possibleactions: ['selectCard'],
+          onState: 'tokenToEnergyPhaseZone',
+          description: _('${actplayer} has to play'),
+          descriptionmyturn: _('${you} have to play'),
           transitions: { done: 201 }
       },
       
@@ -24,3 +24,63 @@ function states()
 
     };
 }
+
+function postSetup() {
+    // Update player names for player zones
+    var players = bga.getPlayers();
+    //* devrait ajouter dans le wiki à quoi ressemble un player dans la bdd & une carte
+    for (var color in players) {
+        player = players[color];
+        //bga.cancel(color);
+        
+        // retrouve l'id de l'élément avec le nom des joueurs pour chaque zone 
+        var labelId = null;
+        if (color == 'ff0000') labelId = bga.getElement( {name: 'Red player'} );
+        if (color == '008000') labelId = bga.getElement( {name: 'Green player'} );
+        if (color == '0000ff') labelId = bga.getElement( {name: 'Blue player'} );
+        if (color == 'ffa500') labelId = bga.getElement( {name: 'Yellow player'} );
+        
+        // crée un tableau avec en clé l'id des éléments "noms de zone" et en valeur le nom du joueur correspondant
+        var props = [];
+        props[labelId] = {name: player.name};
+        // setProperties a l'air de fonctionner avec un tableau de type id => { x: y} avec { x: y} la propriété à setter
+        bga.setProperties( props );
+    }
+    // appelle la transition "done" dans l'état 100
+    bga.nextState('done');
+}
+
+function tokenToEnergyPhaseZone() {
+  var tokenId = bga.getElement( {name: "Phase_token"});
+  var energyPhaseZone = bga.getElement( {name: "Energy_phase_zone"});
+
+  bga.moveTo(tokenId, energyPhaseZone);
+  bga.trace(this.getActivePlayerEnergyPool());
+}
+
+function getActivePlayerEnergyPool() {
+    if (bga.getActivePlayerColor() == 'ff0000') return bga.getElement( {tag: 'ENERGY_POOL_RED'}, 'value');
+    if (bga.getActivePlayerColor() == '008000') return bga.getElement( {tag: 'ENERGY_POOL_GREEN'}, 'value');
+    if (bga.getActivePlayerColor() == '0000ff') return bga.getElement( {tag: 'ENERGY_POOL_BLUE'}, 'value');
+    if (bga.getActivePlayerColor() == 'ffa500') return bga.getElement( {tag: 'ENERGY_POOL_YELLOW'},'value');
+    return null;
+}
+
+
+function checkEndOfGame() {
+    var isGameEnd = (this.getActivePlayerEnergyPool() >= 25);
+
+    if (!isGameEnd) {
+    bga.nextState('done');
+    } else {
+       // End game
+    bga.endGame();
+    }
+}
+
+
+//function endOfTurn() {
+    
+    //bga.nextPlayer();
+    //bga.nextState('done');
+//}
